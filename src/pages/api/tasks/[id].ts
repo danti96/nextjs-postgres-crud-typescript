345ce64427de40c59b243bba91f234519039f430
PROGDESA1 <edisson.alvarado205@gmail.com>
2022-11-30 15:53:47 -0500

    tasks api

---
+++ src/pages/api/tasks/[id].ts
@@ -0,0 +1,16 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+
+export default function (req:NextApiRequest, res: NextApiResponse){
+    const {method} = req;
+    switch (method) {
+        case "GET":
+            return res.status(200).json("Getting a unique task");
+        case "PUT":
+            return res.status(200).json("Updating a unique task");
+        case "DELETE":
+            return res.status(200).json("Deleting a unique task");
+        default:
+            return res.status(200).json("Method not allowed");
+    }
+
+}
