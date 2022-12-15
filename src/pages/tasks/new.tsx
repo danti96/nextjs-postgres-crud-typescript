6e72b06646dac7d3dc458a15a1061f265b05a15c
danti86 <daniel.tintin86@gmail.com>
2022-12-14 22:45:04 -0500

    create tasks

--- src/pages/tasks/new.tsx
+++ src/pages/tasks/new.tsx
@@ -1,20 +1,52 @@
-import React from 'react'
-import { Card, Form, TextArea } from 'semantic-ui-react'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { Button, Card, Form, TextArea, Icon } from 'semantic-ui-react'
+import { Tasks } from "src/interface/Tasks";
 
 export default function newPage() {
+
+    const [task, setTask] = useState({
+        title:"",
+        description:""
+    });
+    const handleChange = ({target:{name, value}}:ChangeEvent<HTMLInputElement|HTMLTextAreaElement>)=>{
+        setTask({...task,[name]:value})
+    }
+
+    const createTask = async (task: Tasks)=>{
+        await fetch('http://localhost:3000/api/tasks',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+    }
+
+    const handleSubmit = (e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        try {
+            createTask(task)
+        } catch (error:any) {
+            console.log(error)
+        }
+    }
     return (
     <div>
         <Card>
             <Card.Content>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Field>
                         <label htmlFor="title">Title: </label>
-                        <input type="text" placeholder="Write your title" name="title"/>
+                        <input type="text" placeholder="Write your title" name="title" onChange={handleChange}/>
                     </Form.Field>
                     <Form.Field>
                         <label htmlFor="description">Description: </label>
-                        <TextArea name="description" id="description" cols={40} rows={4} placeholder="Write your title"></TextArea>
+                        <TextArea name="description" id="description" cols={40} rows={4} placeholder="Write your title" onChange={handleChange}></TextArea>
                     </Form.Field>
+                    <Button>
+                        <Icon name="save"/>
+                        Guardar
+                    </Button>
                 </Form>
             </Card.Content>
         </Card>
