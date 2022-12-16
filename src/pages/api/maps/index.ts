9f28a50d29b5094b7c86109b2b3f9aeeeab960da
PROGDESA1 <edisson.alvarado205@gmail.com>
2022-12-16 15:38:08 -0500

    validate

--- src/pages/api/maps/index.ts
+++ src/pages/api/maps/index.ts
@@ -32,31 +32,35 @@ export default async function handler(
                     adr_address
                   } = req.body;
 
-                const query = 'INSERT INTO mapapi (address, business_status, icon, icon_background_color, icon_mask_base_uri, lat, lng, name, place_id, plus_code_compound_code, plus_code_global_code, rating, telephone, tojson, types, vicinity, website,adr_address)VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18) RETURNING *';
+                if(!existsName(name)){
+                    const query = 'INSERT INTO mapapi (address, business_status, icon, icon_background_color, icon_mask_base_uri, lat, lng, name, place_id, plus_code_compound_code, plus_code_global_code, rating, telephone, tojson, types, vicinity, website,adr_address)VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18) RETURNING *';
 
-                const values = [
-                    address,
-                    business_status,
-                    icon,
-                    icon_background_color,
-                    icon_mask_base_uri,
-                    lat,
-                    lng,
-                    name,
-                    place_id,
-                    plus_code_compound_code,
-                    plus_code_global_code,
-                    rating,
-                    telephone,
-                    toJSON,
-                    types,
-                    vicinity,
-                    website,
-                    adr_address
-                ];
+                    const values = [
+                        address,
+                        business_status,
+                        icon,
+                        icon_background_color,
+                        icon_mask_base_uri,
+                        lat,
+                        lng,
+                        name,
+                        place_id,
+                        plus_code_compound_code,
+                        plus_code_global_code,
+                        rating,
+                        telephone,
+                        toJSON,
+                        types,
+                        vicinity,
+                        website,
+                        adr_address
+                    ];
 
-                const response = await conn.query(query, values);
-                return res.status(200).json(response.rows[0]);
+                    const response = await conn.query(query, values);
+                    return res.status(200).json(response.rows[0]);
+                }
+                return res.status(200).json({message:`Ya existe ${name}`});
+
             } catch (error) {
                 console.log(error)
                 return res.status(200).json(error);
@@ -74,4 +78,17 @@ export default async function handler(
             return res.status(400).json("Invalid method");
     }
 
+}
+async function existsName(nombre:string){
+    try {
+        const query = `SELECT * FROM mapapi where name='${nombre}'`;
+        const response = await conn.query(query);
+        const nrows = response.rows.length;
+        if( nrows > 0){
+            return true;
+        }
+        return false;
+    } catch (error: any) {
+        return true;
+    }
 }
