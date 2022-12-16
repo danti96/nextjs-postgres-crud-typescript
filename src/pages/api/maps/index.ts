b618595fc5e77a79a6740c980bc15dc70f9d7152
PROGDESA1 <edisson.alvarado205@gmail.com>
2022-12-16 13:36:38 -0500

    try catch

--- src/pages/api/maps/index.ts
+++ src/pages/api/maps/index.ts
@@ -10,49 +10,54 @@ export default async function handler(
 
     switch (method) {
         case "POST":
-            const {
-                address,
-                business_status,
-                icon,
-                icon_background_color,
-                icon_mask_base_uri,
-                lat,
-                lng,
-                name,
-                place_id,
-                plus_code_compound_code,
-                plus_code_global_code,
-                rating,
-                telephone,
-                toJSON,
-                types,
-                vicinity,
-                website,
-              } = req.body;
+            try {
+                const {
+                    address,
+                    business_status,
+                    icon,
+                    icon_background_color,
+                    icon_mask_base_uri,
+                    lat,
+                    lng,
+                    name,
+                    place_id,
+                    plus_code_compound_code,
+                    plus_code_global_code,
+                    rating,
+                    telephone,
+                    toJSON,
+                    types,
+                    vicinity,
+                    website,
+                  } = req.body;
 
-            const query = 'INSERT INTO mapapi (address, business_status, icon, icon_background_color, icon_mask_base_uri, lat, lng, "name", place_id, plus_code_compound_code, plus_code_global_code, rating, telephone, tojson, "types", vicinity, website, created_on)VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING *';
-            const values = [ address,
-                business_status,
-                icon,
-                icon_background_color,
-                icon_mask_base_uri,
-                lat,
-                lng,
-                name,
-                place_id,
-                plus_code_compound_code,
-                plus_code_global_code,
-                rating,
-                telephone,
-                toJSON,
-                types,
-                vicinity,
-                website
-            ];
+                const query = 'INSERT INTO mapapi (address, business_status, icon, icon_background_color, icon_mask_base_uri, lat, lng, "name", place_id, plus_code_compound_code, plus_code_global_code, rating, telephone, tojson, "types", vicinity, website, created_on)VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING *';
 
-            const response = await conn.query(query, values);
+                const values = [ address,
+                    business_status,
+                    icon,
+                    icon_background_color,
+                    icon_mask_base_uri,
+                    lat,
+                    lng,
+                    name,
+                    place_id,
+                    plus_code_compound_code,
+                    plus_code_global_code,
+                    rating,
+                    telephone,
+                    toJSON,
+                    types,
+                    vicinity,
+                    website
+                ];
 
-            return res.status(200).json(response.rows[0]);
+                const response = await conn.query(query, values);
+                return res.status(200).json(response.rows[0]);
+            } catch (error) {
+                console.log(error)
+                return res.status(200).json(error);
+            }
 
         case "GET":
             try {
