f6c07dada5cc46c2e3c507e0cf19da839740bc69
PROGDESA1 <edisson.alvarado205@gmail.com>
2022-12-16 11:43:08 -0500

    database render postgresql

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -4,11 +4,11 @@ let conn:any
 
 if(!conn)
     conn = new Pool({
-        user:'postgres',
-        password:'admin',
-        host:'localhost',
+        user:'root',
+        password:'fXtWhF2xfwHTrjHrQNxATdmwdHPPqIc3',
+        host:'postgres://root:fXtWhF2xfwHTrjHrQNxATdmwdHPPqIc3@dpg-cee90d2rrk0c96hgdibg-a/pruebas_ackm',
         port: 5432,
-        database: 'tasksdb'
+        database: 'pruebas_ackm'
     });
 
 console.log(conn)
