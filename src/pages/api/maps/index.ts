12d333c95bd24d6837bc9dd6505c1dd4c914adb6
danti86 <daniel.tintin86@gmail.com>
2022-12-17 20:46:56 -0500

    agregando cors

--- src/pages/api/maps/index.ts
+++ src/pages/api/maps/index.ts
@@ -1,11 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { conn } from 'src/utils/database';
+import NextCors from 'nextjs-cors';
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    // Run the cors middleware
+    // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
+
     const { method } = req;
 
     switch (method) {
