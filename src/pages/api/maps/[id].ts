642e1b52328700ebc6c837bae7822c2ec74a40d9
PROGDESA1 <edisson.alvarado205@gmail.com>
2023-01-03 15:45:41 -0500

    id send require params

--- src/pages/api/maps/[id].ts
+++ src/pages/api/maps/[id].ts
@@ -15,19 +15,19 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         preflightContinue: false,
     });
 
-    const { method, query, body } = req;
+    const { method, query } = req;
 
     switch (method) {
         case "GET":
             try {
-                const text = "SELECT distinct(place_id), * FROM mapapi where code_zip='$1';"
+                const text = "SELECT distinct(place_id), * FROM mapapi where code_zip = $1;"
                 const values = [query.id]
                 const result = await conn.query(text, values);
 
                 if (result.rows.length === 0)
                     return res.status(404).json({ message: "Task not found." });
 
-                return res.status(200).json(result.rows[0]);
+                return res.status(200).json(result.rows);
             } catch (error: any) {
                 return res.status(500).json({ message: error.message })
             }
